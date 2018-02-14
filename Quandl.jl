3cf5a057b565ffc55f39f4f9580865c411146785
Max-Zara <32556415+Max-Zara@users.noreply.github.com>
2018-02-14 08:42:30 -0500

    strategy stats update

--- Quandl.jl
+++ Quandl.jl
@@ -47,7 +47,58 @@ wiki_gs[1,:DayReturn]=0
 wiki_gs[:TradeReturn]=wiki_gs[:Trade] .* wiki_gs[:LnClose] + 1
 wiki_gs[:TradeCapital]=cumprod(wiki_gs[:TradeReturn])
 
+##Basic Statistics
+wiki_gs[:MonthYear]=
+
+
 wiki_gs[:HWM] =
+
+using Plots
+plot(wiki_gs[:TradeCapital],label="Capital ",title ="Return of Buying every 3rd Day")
+
+
+
+###Get Monthly Data
+using Temporal, Base.Dates
+date_array=collect(Dates.today()-Day(365):Day(1):today())
+random_walk = cumsum(randn(length(date_array)))+100.00
+ts_data = TS(random_walk, date_array, :RandomWalk)
+
+
+ts_gs = TS(wiki_gs[:TradeCapital],Array(wiki_gs[:Date]),:TradeCapital)
+#Get the EOM Values
+monthly_gs = eom(ts_gs); annual_gs = eoy(ts_gs)
+month_diff = diff(monthly_gs[:TradeCapital]); annual_diff = diff(annual_gs[:TradeCapital]);
+month_return=rjoin(monthly_gs,month_diff); annual_return=rjoin(annual_gs,annual_diff);
+month_return.fields[2] = :MonReturn; annual_return.fields[2] = :AnnReturn;
+
+#Get the statistics
+strategy_stats = DataFrame(Period = 1, meanreturn =mean(month_return[:MonReturn].values),
+Stdev= std(month_return[:MonReturn].values))
+push!(strategy_stats,[1,mean(annual_return[:AnnReturn].values),mean(annual_return[:AnnReturn].values)])
+
+###Calculate Annual Returns
+
+
+
+
+
+aggregate(month_return[:MonReturn],eoy(),fun=std)
+
+using Plots
+plotlyjs()
+plot(month_return, c=[:black :orange], w=[4 2], subplot=1)
+
+month.sd = std(month_return[:MonReturn])
+
+##Performance Measures
+
+
+
+
+ts_gs = TS(wiki_gs[:TradeCapital],wiki_gs[:Date])
+
+monthly_gs = [monthly_gs 0]
 
 
 #using Plotly
